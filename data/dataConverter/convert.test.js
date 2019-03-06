d9ed28fd8e764e573fb40e34d34b988b607accd5
GraceFrank <frankgracey@gmail.com>
2019-03-06 13:31:04 +0100

    written test case for convert.js

---
+++ data/dataConverter/convert.test.js
@@ -0,0 +1,14 @@
+const convert = require("./convert");
+
+test("that it converts a string to an array of object", () => {
+  const string = `Calista Na Producer`;
+  expect(convert(string)).toEqual([
+    {
+      name: "Calista Na",
+      productNo: 0,
+      stage: "Producer"
+    }
+  ]);
+
+  
+});
