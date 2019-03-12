a4c76201be78f9957bf0d48b3747535269d835cf
Grace Frank <frankgracey@gmail.com>
2019-03-12 08:57:42 +0100

    added test case for is-last.js for when last object has not been reached

--- last-object-status/is-last.test.js
+++ last-object-status/is-last.test.js
@@ -2,16 +2,24 @@ const isLast = require("./is-last");
 
 let first = isLast;
 let second = isLast;
-isLast.setLastObj({ name: "cocoala", id: 55, stage: "Recycler" });
+
+test("that when last object is recycler, status is changed to true", () => {
+  expect(isLast.status()).toBe(false);
+});
+
+
 
 test("that two instances of islast are the same", () => {
+  isLast.setLastObj({ name: "cocoala", id: 55, stage: "Recycler" });
   expect(first === second).toBeTruthy();
 });
 
 test("that after islast is modifeid, it remains the same in any instance", () => {
+  isLast.setLastObj({ name: "cocoala", id: 55, stage: "Recycler" });
   expect(first === isLast).toBeTruthy();
 });
 
 test("that when last object is recycler, status is changed to true", () => {
+  isLast.setLastObj({ name: "cocoala", id: 55, stage: "Recycler" });
   expect(isLast.status()).toBe(true);
 });
